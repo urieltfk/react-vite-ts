58d8559cbe4d98b484be84293d6c0349e83a216e
Uriel <urieltfk@gmail.com>
2024-07-27 22:59:15 +0300

    fixed import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { About } from './views/about'
+import { About } from './views/About'
 import ErrorPage from './views/ErrorPage'
 
 const router = createBrowserRouter([
